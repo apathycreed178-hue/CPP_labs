67564e66dbe36408331ce6524d5731d067a90d52
apathycreed178-hue <apathycreed178@gmail.com>
2026-05-12 23:18:26 +0300

    Add files via upload

---
+++ binary_priority_queue.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "binary_priority_queue.h"
+
+int higherPriority(int p1, int p2);
+
+int main()
+{
+
+    binary_priority_queue queue(higherPriority);
+    
+    queue.insert(13, "aoaoia");
+    queue.insert(1113, "aoa");
+    queue.insert(1, "aoaa");
+    queue.insert(12, "aoaoiaadafa");
+
+    queue.dump();
+
+
+}
+
+
+int higherPriority(int p1, int p2) {
+    return p2 - p1;
+}
