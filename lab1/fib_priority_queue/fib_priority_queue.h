c7048a9a416154e1a46058be5de28a81f89579e1
apathycreed178-hue <apathycreed178@gmail.com>
2026-07-06 09:01:27 +0300

    Update fib_priority_queue.h

--- lab1/fib_priority_queue/fib_priority_queue.h
+++ lab1/fib_priority_queue/fib_priority_queue.h
@@ -3,6 +3,7 @@
 #include "binomial_priority_queue.h"
 #include <iostream>
 #include <stdexcept>
+#include <typeinfo>
 #pragma warning(disable: 4996)
 
 class fib_priority_queue final : public binomial_priority_queue {
@@ -15,15 +16,15 @@ public:
     }
 
     //конструктор копирования
-    fib_priority_queue(const fib_priority_queue &other)
-        : binomial_priority_queue(dynamic_cast<binomial_priority_queue const &>(other)) {
+    fib_priority_queue(const fib_priority_queue& other)
+        : binomial_priority_queue(dynamic_cast<binomial_priority_queue const&>(other)) {
 
     }
 
     //оператор присваивания
     fib_priority_queue& operator=(const fib_priority_queue& other) {
         if (this != &other) {
-            binomial_priority_queue::operator=(dynamic_cast<binomial_priority_queue const &>(other));
+            binomial_priority_queue::operator=(dynamic_cast<binomial_priority_queue const&>(other));
         }
 
         return *this;
@@ -34,12 +35,21 @@ public:
 
     }
 
-    void merge(mergeable_priority_queue * to_merge_from) override {
+    void insert( int priority, char const* value) override {
+		fib_priority_queue new_heap(_priorities_comparer);
+		new_heap._root = create_node(priority, value);
+		new_heap._root->brother = new_heap._root;
+		new_heap._values_count = 1;
+
+		merge(&new_heap);
+	}
+
+    void merge(mergeable_priority_queue* to_merge_from) override {
 
         fib_priority_queue* fib_to_merge_from;
 
-        if ((fib_to_merge_from = dynamic_cast<fib_priority_queue *>(to_merge_from)) == nullptr) {
-            throw std::invalid_argument("Parameter must be fib_priority_queue");
+        if ((fib_to_merge_from = dynamic_cast<fib_priority_queue*>(to_merge_from)) == nullptr) {
+            throw std::invalid_argument("Parameter must be fib_priority_queue*");
         }
 
         if (this == to_merge_from) {
@@ -52,36 +62,32 @@ public:
 
         if (this->_root == nullptr && fib_to_merge_from != nullptr) {
             this->_root = fib_to_merge_from->_root;
-			this->_values_count = fib_to_merge_from->_values_count;
-			fib_to_merge_from->_root = nullptr;
-			fib_to_merge_from->_values_count = 0;
+            this->_values_count = fib_to_merge_from->_values_count;
+            fib_to_merge_from->_root = nullptr;
+            fib_to_merge_from->_values_count = 0;
 
-			return;
+            return;
         }
 
         size_t new_values_count = this->_values_count + fib_to_merge_from->_values_count;
+        fib_to_merge_from->_values_count = 0;
 
-        node* runner = this->_root;
+        node* tmp = this->_root->brother;
+        _root->brother = fib_to_merge_from->_root;
+        node* tmp2 = fib_to_merge_from->_root;
 
-        do {
-            runner = runner->brother;
+        while (tmp2->brother != fib_to_merge_from->_root) {
+            tmp2 = tmp2->brother;
+        }
 
-            if (runner->brother == this->_root) {
-                runner->brother = fib_to_merge_from->_root;
-                runner = fib_to_merge_from->_root;
-            }
+        tmp2->brother = tmp;
 
-            if (runner->brother == fib_to_merge_from->_root) {
-                runner->brother = this->_root;
-                runner = this->_root;
-            }
-
-        } while (runner != this->_root);
-
-        _root = (_priorities_comparer(this->_root->priority, fib_to_merge_from->_root->priority) > 0)
+        this->_root = (_priorities_comparer(this->_root->priority, fib_to_merge_from->_root->priority) > 0)
             ? this->_root : fib_to_merge_from->_root;
+
         _values_count = new_values_count;
         fib_to_merge_from->_values_count = 0;
+        fib_to_merge_from->_root = nullptr;
     }
 
     void consolidate() {
@@ -92,11 +98,23 @@ public:
         fib_priority_queue tmp(*this);
         _values_count = 0;
 
-        binomial_priority_queue *binomial_tmp = static_cast<binomial_priority_queue *>(&tmp);
+        binomial_priority_queue* binomial_tmp = static_cast<binomial_priority_queue*>(&tmp);
         binomial_priority_queue::merge(binomial_tmp);
     }
 
-    char* remove_max() {
+    mergeable_priority_queue* meld(mergeable_priority_queue const* to_meld_with) const override {
+        auto const* fib_to_meld = dynamic_cast<fib_priority_queue const*>(to_meld_with);
+        if (!fib_to_meld) {
+            throw std::invalid_argument("Parameter must be fib_priority_queue const*");
+        }
+
+        fib_priority_queue* result = new fib_priority_queue(*this);  // копия текущей
+        fib_priority_queue temp(*fib_to_meld);                  // копия другой
+        result->merge(&temp);                               // слияние
+        return result;
+    }
+
+    char* remove_max() override{
         if (_root == nullptr) {
             throw std::invalid_argument("Can't delete element from empty queue");
         }
@@ -105,39 +123,40 @@ public:
 
         if (tree_to_split->brother == tree_to_split) {
             _root = nullptr;
-        } else {
-            node *new_max_tree = tree_to_split->brother;
-			node *runner = tree_to_split->brother;
-			while (runner->brother != _root)
-			{
-				if (_priorities_comparer(runner->priority, new_max_tree->priority) > 0)
-				{
-					new_max_tree = runner;
-				}
-				runner = runner->brother;
-			}
-			runner->brother = runner->brother->brother;
-			_root = new_max_tree;
+        }
+        else {
+            node* new_max_tree = tree_to_split->brother;
+            node* runner = tree_to_split->brother;
+            while (runner->brother != _root)
+            {
+                if (_priorities_comparer(runner->priority, new_max_tree->priority) > 0)
+                {
+                    new_max_tree = runner;
+                }
+                runner = runner->brother;
+            }
+            runner->brother = runner->brother->brother;
+            _root = new_max_tree;
         }
 
         _values_count -= (1 << get_rank(tree_to_split));
 
-		fib_priority_queue new_heap(_priorities_comparer);
-		new_heap._root = tree_to_split->children;
-		new_heap._values_count = (1 << get_rank(tree_to_split)) - 1;
+        fib_priority_queue new_heap(_priorities_comparer);
+        new_heap._root = tree_to_split->children;
+        new_heap._values_count = (1 << get_rank(tree_to_split)) - 1;
 
-		char* to_return = tree_to_split->value;
+        char* to_return = tree_to_split->value;
 
-		delete tree_to_split;
+        delete tree_to_split;
 
-		merge(&new_heap);
+        merge(&new_heap);
 
         consolidate();
 
-		return to_return;
+        return to_return;
 
     }
-    
+
 };
 
 #endif
